773dedd8f84885d58e9c22de6b1798204eb06f2e
zookatt <139338863+zookatt@users.noreply.github.com>
2024-03-09 15:13:48 +0100

    Backend server doesn't work properly

--- server/index.js
+++ server/index.js
@@ -16,4 +16,4 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" })
 })
 
-app.listen(8000, () => console.log('Server has started on port 8000'))
+app.listen(8080, () => console.log('Server has started on port 8080'))
